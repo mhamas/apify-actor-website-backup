f4797cbbdc0011ca7dd383fb404fa5d7543a794a
Matej Hamas <matej.hamas@gmail.com>
2020-07-02 14:08:16 +0200

    Changed default logic to simple pupeteer crawler

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-// This is the main Node.js source code file of your actor.
-// It is referenced from the "scripts" section of the package.json file,
-// so that it can be started by running "npm start".
-
 const Apify = require('apify');
 
 Apify.main(async () => {
-    // Get input of the actor.
-    // If you'd like to have your input checked and generate a user interface
-    // for it, add INPUT_SCHEMA.json file to your actor.
-    // For more information, see https://apify.com/docs/actor/input-schema
-    const input = await Apify.getInput();
-    console.log('Input:');
-    console.dir(input);
-
-    // Do something useful here...
-
-    // Save output
-    const output = {
-        receivedInput: input,
-        message: 'Hello sir!',
+    // Create a RequestList
+    const requestList = await Apify.openRequestList('my-list', [
+        { url: 'http://www.example.com/page-1' },
+        { url: 'http://www.example.com/page-2' },
+        { url: 'http://www.example.com/page-3' },
+    ]);
+    // Function called for each URL
+    const handlePageFunction = async ({ request, page }) => {
+        const title = await page.title();
+        console.log(`URL: ${request.url}\nTITLE: ${title}`);
     };
-    console.log('Output:');
-    console.dir(output);
-    await Apify.setValue('OUTPUT', output);
+    // Create a PuppeteerCrawler
+    const crawler = new Apify.PuppeteerCrawler({
+        requestList,
+        handlePageFunction,
+    });
+    // Run the crawler
+    await crawler.run();
 });
