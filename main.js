5426055538ca6498529e91d7cd9bcc72816e274b
Matej Hamas <matej.hamas@gmail.com>
2020-07-03 15:20:59 +0200

    First working version

--- main.js
+++ main.js
@@ -1,22 +1,81 @@
 const Apify = require('apify');
+const { URL } = require('url')
+const { v4: uuidv4 } = require('uuid');
+
+
+function uid_from_url(urlString) {
+    // Only following characters are allowed in keys by Apify platform
+    const allowedCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!-_.\'()/'
+
+    const url = new URL(urlString);
+
+    // Retain only origin and pathname and replace all '/' by '_'
+    let uid = `${url.hostname}${url.pathname}`.replace(/\//g, '_');
+
+    // Filter out characters that are not allowed
+    uid = uid.split('').filter((char) => allowedCharacters.includes(char)).join('')
+
+    // Add unique uuid prefix
+    uid = `${uuidv4()}__${uid}`
+
+    // Return first 256 characters of uid as it's the limit of the Apify platform
+    return uid.slice(0, 256)
+}
 
 Apify.main(async () => {
-    // Create a RequestList
-    const requestList = await Apify.openRequestList('my-list', [
-        { url: 'http://www.example.com/page-1' },
-        { url: 'http://www.example.com/page-2' },
-        { url: 'http://www.example.com/page-3' },
-    ]);
-    // Function called for each URL
+    const {
+        startURLs,
+        maxRequestsPerCrawl,
+        maxConcurrency,
+        linkSelector,
+        sameOrigin
+    } = await Apify.getInput();
+    
+    const requestQueue = await Apify.openRequestQueue();
+    for (const startURL of startURLs) {
+        await requestQueue.addRequest(startURL);
+    }
+
+    const pseudoURLs = [];
+    if (sameOrigin) {
+        for (const startURL of startURLs) {
+            const url = new URL(startURL.url);
+            pseudoURLs.push(`${url.origin}/[.*]`);
+        }
+    }
+
     const handlePageFunction = async ({ request, page }) => {
-        const title = await page.title();
-        console.log(`URL: ${request.url}\nTITLE: ${title}`);
+        const uid = uid_from_url(request.url);
+        console.log(`Creating backup of ${request.url} under id ${uid}`);
+
+        const session = await page.target().createCDPSession();
+        const { data: snapshot } = await session.send('Page.captureSnapshot', { format: 'mhtml' });
+
+        await Apify.setValue(
+            uid,
+            snapshot,
+            {
+                contentType: 'multipart/related'
+            },
+        );
+
+        await Apify.utils.enqueueLinks({
+            page,
+            selector: linkSelector,
+            requestQueue,
+            pseudoUrls: pseudoURLs,
+        });
     };
-    // Create a PuppeteerCrawler
+
     const crawler = new Apify.PuppeteerCrawler({
-        requestList,
+        requestQueue,
         handlePageFunction,
+        maxRequestsPerCrawl,
+        maxConcurrency,
+        launchPuppeteerOptions: {
+            headless: true
+        },
     });
-    // Run the crawler
+
     await crawler.run();
 });
